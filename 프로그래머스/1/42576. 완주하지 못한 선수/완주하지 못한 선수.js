5c79a4f9885848bd5c59bd9361a6e23f05a7c4be
Oh_hong <163826719+Oh-hong@users.noreply.github.com>
2025-08-12 17:07:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 80.54 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,16 @@
+function solution(participant, completion) {
+    var answer = '';
+    const map = {};
+    for(let name of participant) {
+        map[name] = (map[name] || 0) + 1;
+    }
+    for(let name of completion) {
+        map[name]--
+    }
+    for(let name in map) {
+        if(map[name] >= 1) {
+            return name
+        }
+    }
+    return answer;
+}
