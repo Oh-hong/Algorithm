ce93645c10d1fc6e2f023fb5f389420035a791e0
Oh_hong <163826719+Oh-hong@users.noreply.github.com>
2025-09-05 17:16:32 +0900

    [level 1] Title: 폰켓몬, Time: 6.24 ms, Memory: 37.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,12 +1,14 @@
 function solution(nums) {
     var answer = 0;
-    let total = nums.length;
+    const total = nums.length;
     const map = {};
     for(let pocketmon of nums) {
-        map[pocketmon] = (map[pocketmon] || 0) + 1
+     map[pocketmon] = (map[pocketmon] || 0) + 1;
     }
     const number = Object.keys(map)
     const maxSelect = total/2;
+    
     answer = Math.min(number.length, maxSelect)
+   
     return answer;
 }
