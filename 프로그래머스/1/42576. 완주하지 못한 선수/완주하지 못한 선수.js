f01befd56e2106fd18dd6f2ef569c82c891a0674
Oh_hong <163826719+Oh-hong@users.noreply.github.com>
2025-09-05 17:01:36 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 82.52 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,16 +1,36 @@
+// function solution(participant, completion) {
+//     var answer = '';
+//     const map = {};
+//     for(let name of participant) {
+//         map[name] = (map[name] || 0) + 1;
+//         console.log(map)
+//     }
+//     for(let name of completion) {
+//         map[name]--
+//     }
+//     for(let name in map) {
+//         if(map[name] >= 1) {
+//             return name
+//         }
+//     }
+//     return answer;
+// }
+
+
 function solution(participant, completion) {
-    var answer = '';
+    var answer = ''
     const map = {};
     for(let name of participant) {
         map[name] = (map[name] || 0) + 1;
     }
-    for(let name of completion) {
-        map[name]--
+    
+    for (let name of completion) {
+        map[name]--;
     }
-    for(let name in map) {
+    
+    for (let name in map) {
         if(map[name] >= 1) {
             return name
         }
     }
-    return answer;
 }
