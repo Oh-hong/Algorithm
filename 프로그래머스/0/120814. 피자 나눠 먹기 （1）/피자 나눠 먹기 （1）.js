ea5ada7a9fcf9eb251042f958d78e8d2a0df25ee
Oh_hong <163826719+Oh-hong@users.noreply.github.com>
2025-09-09 16:54:40 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,10 +1,5 @@
-//피자 1개에 7조각.
-// 7/n >= 1
 function solution(n) {
     var answer = 0;
-    for(let i = 1; i <= n; i++) {
-        if(i*7/n >= 1) {
-            return i
-        }
-    }
+    answer = n%7 === 0 ? n/7 : parseInt(n/7) +1
+    return answer;
 }
